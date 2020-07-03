1bca3c1c23d3744099c73797bf7be68fa9ff1a7c
Komarov Maksym <komarov.m@phystech.edu>
2020-07-03 17:29:00 +0300

    Refactoring
    1) dirtools: добавлена функция get_agedir: найти папку для данного возраста
    2) imdb_data_preparation теперь содержит только одну функцию (prepare_imdb_data) для подготовки данных. О функции:
    * распаковывает архив tar
    * сортирует по годам с заданным периодом (например: 10 -> папки: [0, 10, 20 ... 90])

--- dirtools.py
+++ dirtools.py
@@ -6,6 +6,7 @@ Created on Wed Jul  1 16:41:51 2020
 """
 import shutil
 import os
+from math import floor
 
 def create_directory(dir_name, classes):
     if os.path.exists(dir_name):
@@ -13,3 +14,12 @@ def create_directory(dir_name, classes):
     os.makedirs(dir_name)
     for i in classes:
         os.makedirs(os.path.join(dir_name, i))
+        
+def get_agedir(apparent_age, classes):
+    if apparent_age < 0 or apparent_age > 99:
+      return 'notsorted'
+    period = 100 // len(classes)
+    apparent_age /= period
+    apparent_age = floor(apparent_age)
+    apparent_age *= period
+    return str(apparent_age)
--- imdb_data_preparation.py
+++ imdb_data_preparation.py
@@ -4,47 +4,66 @@ Created Jun 8 2020
 
 @author: Maksym Komarov
 """
+#Подготовка датасета IMDB. Классы - целые числа в промежутке [0, 99]
+#Любое количество классов с равными промежутками между ними
 from tarfile import TarFile
 import os
 import shutil
 import datetime as dt
 from scipy.io import loadmat
+
+#import sys
+#sys.path.insert(1, '/content/gdrive/My Drive/Age_prediction') #path to dirtools.py
 from dirtools import create_directory
+from dirtools import get_agedir
 
-train_dir = 'second'          #path to locate sorted data
-data_not_sorted = 'imdb_crop' #path to locate not sorted data
-metadata_path = 'imdb_meta'   #path to locate metadata
+#classes = [str(i) for i in range(0, 91, 10)]
+#path_tar_data     = "/content/gdrive/My Drive/Age_prediction/imdb_crop.tar"
+#path_tar_metadata = "/content/gdrive/My Drive/Age_prediction/imdb_meta.tar"
 
-classes = [str(i) for i in range(0, 91, 10)]
+def prepare_imdb_data(period, path_tar_data, path_tar_metadata, \
+        sorted_data_locate, not_sorted_data_locate = None, metadata_locate = None):
+    classes = [str(i) for i in range(0, 99, period)]
+    if not_sorted_data_locate == None:
+      not_sorted_data_locate = 'data_not_sorted_workfile_will_be_deleted'
+    if metadata_locate == None:
+      metadata_locate = 'metadata_workfile_will_be_deleted'
 
-with TarFile("/content/gdrive/My Drive/Age_prediction/imdb_crop.tar","r") as mytar:
-    mytar.extractall(data_not_sorted)
+    train_dir = sorted_data_locate              #path to locate sorted data
+    data_not_sorted = not_sorted_data_locate    #path to locate not sorted data
+    metadata_path = metadata_locate             #path to locate metadata
 
-with TarFile("/content/gdrive/My Drive/Age_prediction/imdb_meta.tar","r") as mytar:
-    mytar.extractall(metadata_path)
+    with TarFile(path_tar_data,"r") as mytar:
+        mytar.extractall(data_not_sorted)
 
-mat = loadmat(metadata_path + '/imdb/imdb.mat')
-#mat['imdb'][0][0][0][0] #dob #days from January 1, 0000
-#mat['imdb'][0][0][1][0] #yearphoto
-#mat['imdb'][0][0][2][0] #path: array([array(['01/nm0000001_rm124825600_1899-5-10_1968.jpg'],
+    with TarFile(path_tar_metadata,"r") as mytar:
+        mytar.extractall(metadata_path)
 
-classes.append('notsorted')
-create_directory(train_dir, classes)
+    mat = loadmat(metadata_path + '/imdb/imdb.mat')
+    #mat['imdb'][0][0][0][0] #dob #days from January 1, 0000
+    #mat['imdb'][0][0][1][0] #yearphoto
+    #mat['imdb'][0][0][2][0] #path: array([array(['01/nm0000001_rm124825600_1899-5-10_1968.jpg'],
+    #classes.append('notsorted')
 
-nclasses = len(classes)
-source_dir = data_not_sorted + '/imdb_crop'
-dest_dir = train_dir
+    create_directory(train_dir, classes)
 
-for i in range(len(mat['imdb'][0][0][1][0])):
-  impath = str(mat['imdb'][0][0][2][0][i][0]) #path to image
-  dob = dt.datetime.fromtimestamp(mat['imdb'][0][0][0][0][i] * 60 * 24 * 60).year - 1970 #date of birth
-  apparent_age = mat['imdb'][0][0][1][0][i] - dob #YEARS OLD!
-  directory = apparent_age // nclasses
-  directory *= nclasses
-  if directory < 0 or directory > 99:
-    directory = 'notsorted'
-  shutil.copy2(os.path.join(source_dir, impath), 
-                    os.path.join(dest_dir, str(directory)))
 
-shutil.rmtree(os.path.join(train_dir, 'notsorted'))
-classes = classes[:-1]
+    source_dir = data_not_sorted + '/imdb_crop'
+    dest_dir = train_dir
+
+    for i in range(len(mat['imdb'][0][0][1][0])):
+      impath = str(mat['imdb'][0][0][2][0][i][0]) #path to image
+      dob = dt.datetime.fromtimestamp(mat['imdb'][0][0][0][0][i] * 60 * 24 * 60).year - 1970 #date of birth
+      apparent_age = mat['imdb'][0][0][1][0][i] - dob #YEARS OLD!
+      if apparent_age >= 0 and apparent_age <= 99:
+        directory = get_agedir(apparent_age, classes)
+        shutil.copy2(os.path.join(source_dir, impath), 
+                          os.path.join(dest_dir, directory))
+
+    #shutil.rmtree(os.path.join(train_dir, 'notsorted'))
+    #classes = classes[:-1]
+
+    if os.path.exists('data_not_sorted_workfile_will_be_deleted'):
+        shutil.rmtree('data_not_sorted_workfile_will_be_deleted')
+    if os.path.exists('metadata_workfile_will_be_deleted'):
+        shutil.rmtree('metadata_workfile_will_be_deleted')
