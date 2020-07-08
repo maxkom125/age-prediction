de4b69f57a912737a3e27c21651897d6f98ba102
maxkom125 <komarov.m@phystech.edu>
2020-07-08 18:36:28 +0300

    Refactoring
    Добавлены новые параметры функции fit_model

--- fit_model.py
+++ fit_model.py
@@ -10,17 +10,22 @@ from keras.applications.vgg16 import VGG16
 from keras.optimizers import Adam
 
 def fit_model(traindata, valdata, fit_type = 'full', epochs = 1, \
-              optimizer = Adam(learning_rate = 1e-5), \
-              loss = 'categorical_crossentropy', image_size = (256, 256)): #fit_type mb fine_tuning
+              optimizer = Adam(learning_rate = 1e-5), dense1units = 512, dense2units = None, \
+              loss = 'categorical_crossentropy', image_size = (256, 256), \
+              output_units = 'auto'): #fit_type mb fine_tuning
+    if output_units == 'auto':
+        output_units = len(traindata.class_indices)
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=(image_size[0], image_size[1], 3))
     model = Sequential()
     model.add(base_model)
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    #model.add(Dense(4096, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
+    if dense1units != None:
+        model.add(Dense(dense1units, activation='relu'))
+        model.add(Dropout(0.5))
+    if dense2units != None:
+        model.add(Dense(dense2units, activation='relu'))
+        model.add(Dropout(0.5))
+    model.add(Dense(output_units, activation='softmax'))
     if fit_type == 'full':
         trainable = True
     else:
